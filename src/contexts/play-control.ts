62f0c087999e8deefee4de9c9407488f252bc460
Cyberproton <cyberprotonatmailbox@gmail.com>
2024-06-01 16:15:10 +0700

    feat(play): play control

--- src/contexts/play-control.ts
+++ src/contexts/play-control.ts
@@ -1,28 +1,27 @@
 import { createContext, useContext } from "react";
 
-export type PlayControlTileProviderProps = {
+export type PlayControlProviderProps = {
   children: React.ReactNode;
 };
 
-export type PlayControlTileState = {
+export type PlayControlState = {
   isPlaying: boolean;
   isHidden: boolean;
   setIsPlaying: (isPlaying: boolean) => void;
   setIsHidden: (isHidden: boolean) => void;
 };
 
-const initialState: PlayControlTileState = {
+const initialState: PlayControlState = {
   isPlaying: false,
   isHidden: true,
   setIsPlaying: () => null,
   setIsHidden: () => null,
 };
 
-export const PlayControlTileContext =
-  createContext<PlayControlTileState>(initialState);
+export const PlayControlContext = createContext<PlayControlState>(initialState);
 
-export const usePlayControlTile = () => {
-  const context = useContext(PlayControlTileContext);
+export const usePlayControl = () => {
+  const context = useContext(PlayControlContext);
 
   if (context === undefined)
     throw new Error("usePlay must be used within a PlayProvider");
